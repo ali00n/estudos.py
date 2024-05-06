01e65ceb69970e0169d5f6186d53a8a7e4700e0e
alisson <alisontattooskate@gmail.com>
2024-05-06 20:58:22 -0300

    novo commit

--- estudo3.py
+++ estudo3.py
@@ -1,10 +1,14 @@
-nummero = int(input("digite um numero positivo maior que um "))
-if numero > 1:
-    for i in range(2, numero):
-        if (numero  % i) == 0:
-            print(f"{numero} nao é um numero primo")
-            break
-    else:
-        print("por favor, insira um numero positivo ")
+numero = int(input("Digite um número positivo maior que 1: "))
+
+if numero <= 1:
+    print("Por favor, insira um número positivo maior do que 1.")
 else:
-    print("por favor, insira um numero positivo maior do que 1 ")
+    primo = True
+    for i in range(2, numero):
+        if numero % i == 0:
+            primo = False
+            break
+    if primo:
+        print(f"{numero} é um número primo.")
+    else:
+        print(f"{numero} não é um número primo.")
